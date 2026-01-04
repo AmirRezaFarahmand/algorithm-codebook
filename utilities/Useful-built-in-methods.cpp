dbf62284d5d4b7cdb723e54bf9100a0b8d163a3c
AmirReza Farahmand <amirreza85farahmand@gmail.com>
2026-01-04 13:40:06 +0330

    Add Useful built-in methods & lowerbound & remove

---
+++ utilities/Useful-built-in-methods.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define F first
+#define S second
+#define all(s) s.begin(), s.end()
+#define pb(s) push_back(s)
+typedef long long int ll;
+typedef long double ld;
+typedef pair <int, int> pii;
+typedef vector <int> vi;
+typedef vector <long long int> vll;
+
+#ifdef LOCAL
+#include "debug.h"
+#else
+#define debug(...) ((void)0)
+#endif
+
+void solve()
+{
+    // Usefull built-in
+
+    // Find lower bound -> x
+    lower_bound(begin, end, x);
+
+    // Remove an element
+    v.erase(remove(v.begin(), v.end(), x), v.end());
+    v.erase(remove_if(v.begin(), v.end(), pred), v.end());
+}   
+
+int main()
+{
+    ios_base :: sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    //cout << fixed << setprecision(3);
+    //freopen("input.txt", "r", stdin);
+    //freopen("output.txt", "w", stdout);
+    //fflush(stdout);
+    int ntc = 1;
+    //cin >> ntc;
+    while (ntc --)
+        solve();
+}
